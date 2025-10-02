bff790bafb0d9a20bfd53b5d243a137a1ea5c3f9
heythem <nh_bouderbala@esi.dz>
2025-10-02 04:48:02 +0100

    blurring and removing the noise

--- code/webcam.py
+++ code/webcam.py
@@ -1,12 +1,15 @@
 import os 
 import cv2
- 
+# Access webcam  
 webcam = cv2.VideoCapture(0)
+# Read frames from webcam
 while True :
     ret,frame = webcam.read()
     if ret :
         cv2.imshow('webcam', frame)
+            # Press 'q' to exit the loop
         if cv2.waitKey(40) & 0xFF == ord('q'):
             break 
+#        
 webcam.release()
 cv2.destroyAllWindows()        
